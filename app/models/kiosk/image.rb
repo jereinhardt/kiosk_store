2f1aa9c16c5f25c4aef1a38da2613d9172ad7816
Josh Reinhardt <joshua.e.reinhardt@gmail.com>
2016-04-09 22:07:10 -0500

    changed image model to update path

--- app/models/kiosk/image.rb
+++ app/models/kiosk/image.rb
@@ -1,6 +1,14 @@
 module Kiosk
   class Image < ActiveRecord::Base
-  	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	has_attached_file :image, 
+      styles: { 
+        medium: "300x300>", 
+        thumb: "100x100>" 
+      }, 
+      :storage => :s3,
+      :s3_credentials => "#{Rails.root}/config/s3.yml",
+      :path => ":attachment/:id/:style.:extension",
+      :url => ':s3_domain_url'
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   	belongs_to :product
